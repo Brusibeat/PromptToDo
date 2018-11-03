00623675e66e390bae88c94113cc07e6e3070892
Roberto "ResaloliPT" Pires <roberto_pires97@sapo.pt>
2018-11-02 23:16:12 -0100

    Created IntelliJ Project && Added Hello World for Client&Server

---
+++ src/org/academiadecodigo/hashtronauts/client/ClientMain.java
@@ -0,0 +1,7 @@
+package org.academiadecodigo.hashtronauts.client;
+
+public class ClientMain {
+    public static void main(String[] args) {
+        System.out.println("Hello From Client");
+    }
+}
---
+++ src/org/academiadecodigo/hashtronauts/server/ServerMain.java
@@ -0,0 +1,7 @@
+package org.academiadecodigo.hashtronauts.server;
+
+public class ServerMain {
+    public static void main(String[] args) {
+        System.out.println("Hello From Server.");
+    }
+}
